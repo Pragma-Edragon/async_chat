6db0b302e041c91915f8cc8788246f9707a4271e
r1v3n <rivenfornotifications@mail.ru>
2021-01-13 16:46:50 +0300

    Now on asyncio.Protocol # TODO: make client with no PROTOCOl

--- client.py
+++ client.py
@@ -1,39 +1,60 @@
-import threading
-import socket
+import asyncio
+import logging
+import functools
 import sys
 
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(name)s: %(message)s',
+    stream=sys.stdout
+)
 
-def get_attr():
-    # getting ARGV args with sys.argv
-    if sys.argv[1] is None and sys.argv[2] is None:
-        raise KeyError("HOST and PORT must be set!")
-    return sys.argv[1], sys.argv[2]
-
-
-# creating socket IP/V4 TCP object
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-HOST, PORT = get_attr()
-sock.connect((HOST, int(PORT)))
+SERVER_ADDRESS = ('localhost', 8080)
 
 
-def send_to_server():
-    sock.send('Hello, world!'.encode())
-    while True:
-        print(":")
-        message = sys.stdin.readline()
-        if len(message) <= 0:
-            quit()
-        sock.send(message.encode())
+class ClientClass(asyncio.Protocol):
 
+    def __init__(self, future, message=None):
+        self.log = logging.getLogger('client')
+        self.message = message
+        self.f = future
 
-def receive_from_server():
-    while True:
-        data = sock.recv(1024)
-        if data:
-            print(data.decode())
+    def connection_made(self, transport) -> None:
+        self.transport = transport
+        self.address = self.transport.get_extra_info('peername')
+        self.log.debug("Connection made to: {:s}:{:d}".format(*self.address))
+        self.transport.write("Hello, world!".encode())
+        print(self.ainput())
+        if self.transport.can_write_eof():
+            self.transport.write_eof()
+
+    def data_received(self, data: bytes) -> None:
+        self.log.debug("Message: {!r}".format(data))
+
+    def connection_lost(self, exc) -> None:
+        self.transport.close()
+        if not self.f.done():
+            self.f.set_result(True)
+        super().connection_lost(exc)
+
+    async def ainput(self):
+        return await asyncio.get_event_loop().run_in_executor(
+            None, sys.stdin.readline)
 
 
 if __name__ == '__main__':
-    threading_send = threading.Thread(target=send_to_server).start()
-    threading_recv = threading.Thread(target=receive_from_server).start()
+    loop = asyncio.get_event_loop()
+    client_completed = asyncio.Future()
+    client_factory = functools.partial(
+        ClientClass,
+        future=client_completed
+    )
+    factory_coro = loop.create_connection(
+        client_factory,
+        *SERVER_ADDRESS,
+    )
+    try:
+        loop.run_until_complete(factory_coro)
+        loop.run_until_complete(client_completed)
+    finally:
+        loop.close()
--- server.py
+++ server.py
@@ -1,47 +1,80 @@
 import asyncio
+import logging
+import sys
 
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(name)s: %(message)s',
+    stream=sys.stderr
+)
+
+log = logging.getLogger('server')
 connections = []
 
 
-async def handle_server(reader, writer):
-    while True:
-        try:
-            data = await reader.read(1024)
-            message = data.decode()
-            client = writer.get_extra_info('peername')
+class ConnectionServer(asyncio.Protocol):
 
-            if len(connections) > 0:
-                for user in connections:
-                    if user != writer:
-                        user.write(
-                            ("<{:s}:{:d}>\t".format(*client) + message).encode()
-                        )
-                        await user.drain()
+    def connection_made(self, transport) -> None:
+        # Each client connection triggers this method
+        # :transport - instance of asyncio
+        # "Transport, which provides an abstraction
+        # for doing asynchronous I/O using the socket".
+        #
+        self.transport = transport
+        self.address = "{:s}:{:d}".format(*self.transport.get_extra_info('peername'))
+        connections.append(self)
 
-            if writer not in connections:
-                connections.append(writer)
-                for user in connections:
-                    user.write("[root] New client arrived: {:s}:{:d}".format(*client).encode())
+        log.debug('Connection from: {}'.format(self.address))
 
-            print(connections)
-        except ConnectionResetError as e:
-            print(e)
-            for user in connections:
-                user.write("<{:s}:{:d}> disconnected".format(*user.get_extra_info('peername')).encode())
-            pass
+        # need all users receive this
+        # connections contains
+        # list of objects
+        for user in connections:
+            if user != self:
+                self.send("<N> user arrived!".encode())
 
-loop = asyncio.get_event_loop()
-coro = asyncio.start_server(
-    handle_server, '127.0.0.1', 8080, loop=loop
-)
-server = loop.run_until_complete(coro)
+    def data_received(self, data: bytes) -> None:
+        # After establishing connection this method
+        # will receive and store data;
+        #
+        # After - sending response to client
+        # that requested any type of data
 
-print("<Serving on: localhost:8080>")
-try:
-    loop.run_forever()
-except KeyboardInterrupt:
-    quit()
+        # need to register /whisper command.
+        # /whisper <username or hostname>
+        log.debug("Message: {}:\t{}".format(self.address, data.decode()))
+        for user in connections:
+            if user != self:
+                self.send(data)
 
-server.close()
-loop.run_until_complete(server.wait_closed())
-loop.close()
+    def connection_lost(self, error) -> None:
+        # When connection is closed
+        # either normally or with error,
+        # this method called
+
+        # if error occurred - exc contains
+        # exception information in () format
+        log.debug("Connection lost from: {}".format(self.address))
+
+        connections.remove(self)
+        super().connection_lost(error)
+
+    def send(self, message: bytes) -> None:
+        self.transport.write(message)
+
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+
+    # getting coroutine object
+    log.debug("Serving server on: <localhost:8080>")
+    coro = loop.create_server(ConnectionServer, host='localhost', port=8080)
+
+    # run the Future to get it's result
+    server = loop.run_until_complete(coro)
+    try:
+        loop.run_forever()
+    finally:
+        server.close()
+        loop.run_until_complete(server.wait_closed())
+        loop.close()
